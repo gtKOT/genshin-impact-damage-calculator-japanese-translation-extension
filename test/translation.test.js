0ad93fd60fcc7f4a07b3db6ff96638901d639a9d
Kota Kimura <askastr@gmail.com>
2021-02-28 21:18:38 +0900

    test: 翻訳にドット無しのLv表記（Lv+3, Lv15など）を使わない

--- test/translation.test.js
+++ test/translation.test.js
@@ -9,3 +9,9 @@ test.each(
 )('一部の全角記号・全角数字を含まないこと(%s)', (key, translation) => {
   expect(translation).not.toMatch(/[％＋－０１２３４５６７８９]/);
 });
+
+test.each(
+  translationEntries
+)('ドット無しのLv表記（Lv+3, Lv15など）を含まないこと(%s)', (key, translation) => {
+  expect(translation).not.toMatch(/Lv\+?[0-9]+/);
+});
